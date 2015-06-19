5bb52b2e3476ef74a6730c1356460e717d8422ea
DimSan4ever <dis-ne@ukr.net>
2015-06-19 19:06:16 +0300

    Update mysqlHome.sql

--- mysqlHome.sql
+++ mysqlHome.sql
@@ -19,3 +19,10 @@ group by `order`.user_id
 having sum(`order`.amount)
 order by `order`.amount desc 
 limit 1);
+
+#книги 1 и 3
+
+select library.autor_id,GROUP_CONCAT(library.book_id) Books
+from library
+group by library.autor_id
+having Books = "1,3";
