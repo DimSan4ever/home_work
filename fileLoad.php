bb3eca2019101067698151e55b1825dd80e56213
DimSan4ever <dis-ne@ukr.net>
2015-07-04 01:15:31 +0300

    Update fileLoad.php

--- fileLoad.php
+++ fileLoad.php
@@ -4,20 +4,26 @@
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body style="text-align: center">       
+    <body style="text-align: center">
+       
         <form method="post" enctype="multipart/form-data">
            <label for="file"><h2>load image</h2></label>
             <input id="file" name="file" type="file">
+            <label for="file2"><h2>load file</h2></label>
+            <input id="file2" name="file2" type="file">
             <p><input type="submit"></p>
         </form>
         
         <?php
         if(isset($_FILES['file']))
             {
-            if($_FILES['file']['error']==0)
-                {            
-                move_uploaded_file($_FILES['file']['tmp_name'],'img/'.$_FILES['file']['name']);            
-                }        
+            foreach ($_FILES as $k=>$v){                
+                if($v['error']==0)
+                    {            
+                    move_uploaded_file($v['tmp_name'],'img/'.$v['name']); 
+                    echo '<img src="/homework/img/'.$v['name'].'" alt="'.$k.'">';                    
+                    }  
+                }
             }
         ?>
     </body>
